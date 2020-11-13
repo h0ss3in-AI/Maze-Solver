5b07f0aca67a91cd717e3f7b75cde085cea0f5b9
Hossein Hashemi <me@hossein-hashemi.com>
2020-11-13 20:57:28 +0330

    add methods of base frontier

--- entities/base_frontier.py
+++ entities/base_frontier.py
@@ -1,3 +1,15 @@
 class BaseFrontier:
     def __init__(self):
         self.frontier = []
+
+    def add(self):
+        pass
+
+    def remove(self):
+        pass
+
+    def is_empty(self):
+        return len(self.frontier) == 0
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
